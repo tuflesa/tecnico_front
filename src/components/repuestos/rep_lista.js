46470c056443d958b8dd318acd10e925ae058f7a
merce-dom <mdomenejuan@gmail.com>
2022-02-14 08:21:39 +0100

    listados ordenados

--- src/components/repuestos/rep_lista.js
+++ src/components/repuestos/rep_lista.js
@@ -31,7 +31,15 @@ const RepLista = () => {
                   }
             })
             .then( res => {
-                setRepuestos(res.data);
+                setRepuestos(res.data.sort(function(a, b){
+                    if(a.nombre > b.nombre){
+                        return 1;
+                    }
+                    if(a.nombre < b.nombre){
+                        return -1;
+                    }
+                    return 0;
+                }));
             })
             .catch( err => {
                 console.log(err);
