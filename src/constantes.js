c551ebb6e46473d13c1c0bf41c8e760033b731fe
tuflesa <tecnico@tuflesa.es>
2022-06-02 15:55:04 +0200

    constantes

--- src/constantes.js
+++ src/constantes.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export const BACKEND_SERVER = 'http://10.128.100.242:8000' // Producción
 // export const BACKEND_SERVER = 'http://127.0.0.1:8000' // Test
-=======
-//export const BACKEND_SERVER = 'http://10.128.100.242:8000' // Producción
-export const BACKEND_SERVER = 'http://127.0.0.1:8000' // Test
->>>>>>> dev
