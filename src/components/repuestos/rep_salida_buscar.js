81237ef4eb82ac763e30213837e65ee5e494549d
Mercedes Domene <mercedes.domene@bornay.es>
2023-01-30 09:34:39 +0100

    paginación con botones en salida y traspaso de almacén

--- src/components/repuestos/rep_salida_buscar.js
+++ src/components/repuestos/rep_salida_buscar.js
@@ -6,17 +6,23 @@ import Modal from 'react-bootstrap/Modal'
 import { Button, Row, Form, Col, Table } from 'react-bootstrap';
 import { ArrowDownCircle} from 'react-bootstrap-icons';
 import { local } from 'd3';
+
+//import TablePagination from '@mui/material/TablePagination';
+
 const BuscarRepuestos = ({cerrarListRepuestos, show, almacen, elegirRepuesto})=>{
     const [token] = useCookies(['tec-token']);
     const [filtroII, setFiltroII] = useState(`?stocks_minimos__almacen__id=${almacen}`);
     const [repuesto, setRepuesto] = useState(null);
-    //const [localizaciones, setLocalizaciones] = useState(null);
+    const [count, setCount] = useState(null);
     const [filtro, setFiltro] = useState( '');
     const [buscando, setBuscando] = useState(false);
+    //var pagina = 1;
+
     const [datos, setDatos] = useState({
         id:'',
         nombre: '',  
         nombre_comun: '',   
+        pagina: 1,
     });
 
     const actualizaFiltro = str => {
@@ -24,17 +30,15 @@ const BuscarRepuestos = ({cerrarListRepuestos, show, almacen, elegirRepuesto})=>
     }
 
     useEffect(()=>{
-        console.log('buscando');
-        console.log(buscando);
         if (!buscando){
             setFiltro(filtroII);
         }
     },[buscando, filtroII, show]);
   
     useEffect(()=>{
-        const filtro2 = `?stocks_minimos__almacen__id=${almacen}&nombre__icontains=${datos.nombre}&id=${datos.id}&nombre_comun__icontains=${datos.nombre_comun}`;
+        const filtro2 = `?stocks_minimos__almacen__id=${almacen}&page=${datos.pagina}&nombre__icontains=${datos.nombre}&id=${datos.id}&nombre_comun__icontains=${datos.nombre_comun}`;
         actualizaFiltro(filtro2);
-    },[datos.id, datos.nombre, datos.nombre_comun, almacen]);
+    },[datos.id, datos.nombre, datos.nombre_comun, almacen, datos.pagina]);
 
     useEffect(()=>{
         if (filtro){
@@ -44,9 +48,11 @@ const BuscarRepuestos = ({cerrarListRepuestos, show, almacen, elegirRepuesto})=>
                     'Authorization': `token ${token['tec-token']}`
                 }     
             })
-            .then( res => {  
-                console.log(res.data);
+            .then( res => { 
                 setRepuesto(res.data.results);
+                setCount(res.data.count);
+                datos.next=(res.data.next);
+                datos.previous=res.data.previous;
                 setBuscando(false);
             })
             .catch(err => { console.log(err);})
@@ -58,7 +64,29 @@ const BuscarRepuestos = ({cerrarListRepuestos, show, almacen, elegirRepuesto})=>
             ...datos,
             [event.target.name] : event.target.value
         })
-    }  
+    } 
+    
+    const cambioPagina = (pag) => {
+        if(pag<=0){
+            pag=1;
+        }
+        if(pag>count/3){
+            if(count % 3 === 0){
+                pag=Math.trunc(count/3);
+            }
+            if(count % 3 !== 0){
+                pag=Math.trunc(count/3)+1;
+            }
+        }
+        console.log(Math.trunc(count/3));
+        console.log(pag);
+        if(pag>0){
+            setDatos({
+                ...datos,
+                pagina: pag,
+            })
+        }
+    } 
    
     return(
         <Modal show={show} backdrop="static" keyboard={ false } animation={false} size="xl">
@@ -98,6 +126,7 @@ const BuscarRepuestos = ({cerrarListRepuestos, show, almacen, elegirRepuesto})=>
                         </Form.Group>
                     </Col>
                 </Row>
+                    
             </Modal.Header>
             <Modal.Body>
                 <Row>
@@ -132,11 +161,26 @@ const BuscarRepuestos = ({cerrarListRepuestos, show, almacen, elegirRepuesto})=>
                     </Col>
                 </Row>
             </Modal.Body>
-            <Modal.Footer>                                               
-                    <Button variant="info" onClick={cerrarListRepuestos}>
-                        Cerrar
-                    </Button>
-                </Modal.Footer>
+            <Modal.Footer>
+                <table>
+                    <tbody>
+                        <th><button type="button" class="btn btn-default" value={datos.pagina} name='pagina_anterior' onClick={event => {cambioPagina(datos.pagina=datos.pagina-1)}}>Pág Anterior</button></th> 
+                        <th><button type="button" class="btn btn-default" value={datos.pagina} name='pagina_posterior' onClick={event => {cambioPagina(datos.pagina=datos.pagina+1)}}>Pág Siguiente</button></th> 
+                        {/* <th>
+                            {repuesto && repuesto.map( rep => {                                    
+                                return (                                 
+                                    <th key={rep.id}>
+                                        <td><button type="button" class="btn btn-default">{rep.id}</button></td>                                       
+                                    </th>
+                                )})
+                            }
+                        </th> */}
+                    </tbody>
+                </table>
+                <Button variant="info" onClick={cerrarListRepuestos}>
+                    Cerrar
+                </Button>
+            </Modal.Footer>
         </Modal>    
     )
 }
