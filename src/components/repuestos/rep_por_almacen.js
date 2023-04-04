f77eeaed37bab24ec2d8444e93ad45ee0d721bbf
Mercedes Domene <mercedes.domene@bornay.es>
2023-04-04 08:18:37 +0200

    aceptar cambio en rep_por_almacen

--- src/components/repuestos/rep_por_almacen.js
+++ src/components/repuestos/rep_por_almacen.js
@@ -300,7 +300,6 @@ const RepPorAlmacen = ({empresa, repuesto, setRepuesto, cerrarListAlmacen, show}
                                                     <td>{pedidos.por_recibir}</td>
                                                     <td>{pedidos.pedido.proveedor.nombre}</td> 
                                                     <td>{invertirFecha(String(pedidos.pedido.fecha_prevista_entrega))}</td>                                     
-<<<<<<< HEAD
                                                     {!nosoyTecnico?
                                                         <td>
                                                             <Link to={`/repuestos/pedido_detalle/${pedidos.pedido.id}`}>
@@ -308,11 +307,6 @@ const RepPorAlmacen = ({empresa, repuesto, setRepuesto, cerrarListAlmacen, show}
                                                             </Link>
                                                         </td>
                                                     :null}
-=======
-                                                    <td>
-                                                        <Link to={`/repuestos/pedido_detalle/${pedidos.pedido.id}`}><PencilFill className="mr-3 pencil"/></Link>
-                                                    </td>
->>>>>>> proveedores
                                                 </tr>
                                             )})
                                         }
