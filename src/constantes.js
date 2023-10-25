f5124371f05b855bc3c5280d38e33e54c55bd941
Mercedes Domene <mercedes.domene@bornay.es>
2023-10-25 08:31:40 +0200

    cambio de constantes para subir al servidor

--- src/constantes.js
+++ src/constantes.js
@@ -1,2 +1,2 @@
-// export const BACKEND_SERVER = 'http://10.128.100.242:8000' // Producción
-export const BACKEND_SERVER = 'http://127.0.0.1:8000' // Test
+export const BACKEND_SERVER = 'http://10.128.100.242:8000' // Producción
+//export const BACKEND_SERVER = 'http://127.0.0.1:8000' // Test
