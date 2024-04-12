52ce0562785cba769691768b9672f9967e09023c
tuflesa <tecnico@tuflesa.es>
2024-04-12 16:42:38 +0200

    Cargas: cambio en cargas lista y index.js para entrar en llamadas sin login

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ import Mantenimiento from './components/mantenimiento/mantenimiento';
 import Rodillos from './components/rodillos/rodillos';
 import { CookiesProvider, useCookies } from 'react-cookie';
 import QS from './components/qs/qs';
+import CargasLista from './components/cargas/cargas_carga_lista';
+import LlamadasLista from './components/cargas/cargas_llamadas_lista';
 
 const Render = () => {
   const [token] = useCookies(['tec-token']);
@@ -64,6 +66,9 @@ const Render = () => {
             </Route>
           <Route path='/qs'>
             {token['tec-token'] ? <QS /> : <Redirect to="/" />}
+          </Route>
+          <Route path='/llamadas'>
+            <LlamadasLista />
           </Route>
         </Switch>
       </BrowserRouter>
