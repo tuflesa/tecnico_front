f9c8a708f604c32ef31f1c784f5f0fc7407bce3b
tuflesa <tecnico@tuflesa.es>
2024-12-31 14:11:23 +0100

    avance en la aplicación QS de producción

--- src/components/qs/qs_produccion.js
+++ src/components/qs/qs_produccion.js
@@ -4,56 +4,78 @@ import { Container, Row, Col, Form} from 'react-bootstrap';
 import { useCookies } from 'react-cookie';
 import { BACKEND_SERVER } from '../../constantes';
 import QSNavBar from "./qs_nav";
+import StandChart from "./qs_stand_chart";
 
 const QS_Produccion = () => {
     const [token] = useCookies(['tec-token']);
     const [grupos, setGrupos] = useState(null);
     const [grupo, setGrupo] = useState(0);
     const [montajes, setMontajes] = useState(null);
-    const [monatje, setMontaje] = useState([]);
+    const [montaje, setMontaje] = useState(null);
     const [montajeActivo, setMontajeActivo] = useState(0);
+    const [OP, setOP] = useState(1);
+    const [ejes, setEjes] = useState(null);
+    const [posiciones, SetPosiciones] = useState(null);
+
+    const leerEjes = (event) => {
+        // event.preventDefault();
+        axios.get(BACKEND_SERVER + '/api/qs/ejes/',{
+            headers: {
+                'Authorization': `token ${token['tec-token']}`
+                }
+        })
+        .then( res => {
+                setEjes(res.data);
+                // console.log('ejes:');
+                // console.log(res.data);
+        })
+    }
 
     const LeeMontaje = (dato) => {
-        console.log(dato);
         const temp = []; // Aqui guardo el montaje temporal
+        const bancadas = dato.grupo.bancadas;
+        bancadas.push(dato.bancadas);
+        console.log(bancadas);
         // Seccion formadora
-        const formadora = dato.grupo.bancadas.filter(b => b.seccion.nombre=='Formadora')[0];
-        console.log(formadora);
-        formadora.celdas.map(c => {
-            const tipo = c.operacion.nombre.substr(0,2)=='BD'?'BD':c.operacion.nombre;
-            const rod = c.conjunto.elementos.map(e => {
-                return ({
-                tipo_plano: 'BD_W_INF',
-                eje: 'INF',
-                parametros: {
-                    Ancho: 660,
-                    Dext: 550,
-                    Df: 300,
-                    Dc: 338.5,
-                    R1: 126,
-                    alfa1: 63,
-                    xc1: 155.3,
-                    R2: 489,
-                    alfa2: 12,
-                    R3:15
-                }})
-            });
-            temp.push({
-                operacion: c.operacion.orden,
-                color: 'blue',
-                tipo: tipo,
-                nombre: c.operacion.nombre,
-                rodillos: rod
+        // const formadora = dato.grupo.bancadas.filter(b => b.seccion.nombre=='Formadora')[0];
+        // console.log(formadora);
+        bancadas.map(b => {
+            b.celdas.map(c => {
+                const tipo = c.operacion.nombre.substr(0,2)=='BD'?'BD':c.operacion.nombre;
+                const rod = c.conjunto.elementos.map(e => {
+                    const instancia = e.rodillo.instancias.filter(i => i.activa_qs==true)[0] // Instancia activa
+                    // Parametros de la instancia
+                    const param = {
+                        Ancho: instancia.ancho,
+                        Df: instancia.diametro,
+                        Dext: instancia.diametro_ext,
+                        Dc: instancia.diametro_centro
+                    }
+                    // Añadimos parametros del rodillo
+                    e.rodillo.parametros.map(p => param[p.nombre]=p.valor);
+                    return ({
+                    tipo_plano: e.rodillo.tipo_plano?e.rodillo.tipo_plano.nombre:'NONE',
+                    eje: e.eje.tipo.siglas,
+                    parametros: param
+                })
+                });
+                temp.push({
+                    operacion: c.operacion.orden,
+                    color: 'blue',
+                    tipo: tipo,
+                    nombre: c.operacion.nombre,
+                    rodillos: rod
+                });
             });
         });
-        console.log(temp);
+        setMontaje(temp.sort((a,b) => a.operacion - b.operacion))
     }
     
     const handleGrupoChange = (event) => {
         event.preventDefault();
         setGrupo(event.target.value);
         setMontajeActivo(0);
-        setMontaje([]);
+        setMontaje(null);
     }
 
     const handleMontajeChange = (event) => {
@@ -62,6 +84,11 @@ const QS_Produccion = () => {
         setMontajeActivo(event.target.value);
         const montaje_id = event.target.value;
         LeeMontaje(montajes.filter(m => m.id==montaje_id)[0]);
+    }
+
+    const handleInputChange = (event) => {
+        event.preventDefault();
+        setOP(event.target.value);
     }
 
     // Lectura de grupos
@@ -77,6 +104,7 @@ const QS_Produccion = () => {
         .catch( err => {
             console.log(err);
         });
+        leerEjes();
     }, [token]);
 
     // Lectura de montajes del grupo
@@ -94,6 +122,89 @@ const QS_Produccion = () => {
             console.log(err);
         });
     }, [token, grupo]);
+
+    // useEffect(()=>{
+    //     const pos = [];
+    //     let eje_sup;
+    //     let eje_inf;
+    //     let Ds;
+    //     let Di;
+    //     let gap;
+    //     let piston;
+    //     const gap_list = [];
+
+    //     if (ejes && montaje){
+    //         // Calculo de posiciones
+    //         montaje.map(m => {
+    //             const line = [];
+    //             m.rodillos.map((r,i) =>{
+    //                 switch (r.eje){
+    //                     // case 'LAT_MO':
+    //                     // case 'LAT_OP':
+    //                     //     line.push({
+    //                     //         eje: r.eje,
+    //                     //         pos: 492 -r.parametros.Df/2 - ejes[m.operacion-1].pos[r.eje],
+    //                     //         pos_sim: 492 -r.parametros.Df/2 - ejesSim[m.operacion-1].pos[r.eje]
+    //                     //     });
+    //                     //     break;
+    //                     case 'SUP_V_MO':
+    //                     case 'SUP_V_OP':
+    //                         line.push({
+    //                             eje: r.eje,
+    //                             pos: -r.parametros.Df/2 + (465 - ejes[m.operacion-1].pos[r.eje])/Math.cos(15*Math.PI/180),
+    //                         });
+    //                         console.log(r.eje);
+    //                         console.log('Df: ', r.parametros.Df/2);
+    //                         console.log('Eje: ', ejes[m.operacion-1].pos[r.eje]);
+    //                         console.log('sup: ', -r.parametros.Df/2 + (465 - ejes[m.operacion-1].pos[r.eje])/Math.cos(15*Math.PI/180));
+    //                         break;
+    //                     case 'SUP_H_MO':
+    //                     case 'SUP_H_OP':
+    //                         line.push({
+    //                             eje: r.eje,
+    //                             pos: 138.183 - ejes[m.operacion-1].pos[r.eje] - Math.sin(15*Math.PI/180)*r.parametros.Df/2,
+    //                         });
+    //                         break;
+    //                     case 'INF_W':
+    //                         line.push({
+    //                             eje: r.eje,
+    //                             pos: -r.parametros.Df/2 + ejes[m.operacion-1].pos[r.eje] + ejes[m.operacion-1].pos['CAB'] - 298.5,
+    //                         });
+    //                         break;
+    //                     case 'ANCHO_S1':
+    //                     case 'ANCHO':
+    //                         line.push({
+    //                             eje: r.eje,
+    //                             pos: 170 + ejes[m.operacion-1].pos[r.eje] -r.parametros.Df,
+    //                         });
+    //                         break;
+    //                     case 'ALTO':
+    //                         line.push({
+    //                             eje: r.eje,
+    //                             pos: -270 + ejes[m.operacion-1].pos[r.eje],
+    //                         });
+    //                     break;
+    //                     case 'ALTO_S1':
+    //                         line.push({
+    //                             eje: r.eje,
+    //                             pos: -40 + ejes[m.operacion-1].pos[r.eje],
+    //                         });
+    //                     break;  
+    //                     default: 
+    //                         line.push({
+    //                             eje: r.eje,
+    //                             pos: -r.parametros.Df/2 + ejes[m.operacion-1].pos[r.eje],
+    //                         });
+    //                 }
+    //             });
+    //             pos.push({
+    //                 op: m.operacion,
+    //                 posiciones: line
+    //             });
+    //         });
+    //         SetPosiciones(pos);
+    //     }
+    // },[montaje, ejes]);
 
     return (
         <React.Fragment>
@@ -141,6 +252,34 @@ const QS_Produccion = () => {
                                 </Form.Group>
                             </Col>
                         </Row>
+                        <Row>
+                            <Col>
+                                <Form.Group controlId="operacion">
+                                    <Form.Control as="select" 
+                                                    value={OP}
+                                                    name='operacion'
+                                                    onChange={handleInputChange}>
+                                        {montaje && montaje.map( m => {
+                                            return (
+                                            <option key={m.operacion} value={m.operacion}>
+                                                {m.nombre}
+                                            </option>
+                                            )
+                                        })}
+                                    </Form.Control>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        {/* <Row>
+                            <Col className="col-6">
+                                {montaje&&posiciones&&ejes&&<StandChart montaje={montaje&&montaje.filter(m => m.operacion == OP)}
+                                            ejes={ejes&&ejes.filter(e => e.op == OP)[0].pos}
+                                            posiciones={null}//posiciones&&posiciones.filter(p => p.op==OP)[0].posiciones}
+                                            simulador={false}
+                                            gap = {null}
+                                            fleje={null}/>  }
+                            </Col>
+                        </Row> */}
                     </Form>
                 </Row>
             </Container>
