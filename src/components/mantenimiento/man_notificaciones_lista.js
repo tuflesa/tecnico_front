118944b024e6073e7105993850e280a80b9d5b30
Mercedes Domene <mercedes.domene@bornay.es>
2025-09-02 08:32:39 +0200

    Mantenimiento: corregir filtros notificaciones

--- src/components/mantenimiento/man_notificaciones_lista.js
+++ src/components/mantenimiento/man_notificaciones_lista.js
@@ -11,7 +11,7 @@ const ManNotificacionesLista = () => {
     const [user] = useCookies(['tec-user']);
     
     const [notas, setNotas]  = useState(null);
-    const [filtroBase, setFiltroBase] = useState(`?empresa__id=${user['tec-user'].perfil.empresa.id}`);
+    const [filtroBase, setFiltroBase] = useState(`?empresa__id=${user['tec-user'].perfil.empresa.id}&finalizado=${false}&descartado=${false}`);
     const [count, setCount] = useState(null);
     const [buscando, setBuscando] = useState(false);
     const [pagina, setPagina] = useState(1);
