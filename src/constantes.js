46478d40f087d695233803e89e22c99d5b26fd95
Mercedes Domene <mercedes.domene@bornay.es>
2025-12-05 13:26:24 +0100

    Constantes: conflicto con las constantes.js

--- src/constantes.js
+++ src/constantes.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-// export const BACKEND_SERVER = 'http://10.128.100.242:8000' // Producción
-=======
-//export const BACKEND_SERVER = 'http://10.128.100.242:8000' // Producción
->>>>>>> 622f43b66e17c9a651b35fafc5eb641a8254b714
-export const BACKEND_SERVER = 'http://127.0.0.1:8000' // Test
+export const BACKEND_SERVER = 'http://10.128.100.242:8000' // Producción
+//export const BACKEND_SERVER = 'http://127.0.0.1:8000' // Test
